472f45e9c7b6466a0048caf77aa3c21b6c148fd3
Sarah Berenji <sarahba@kth.se>
2015-10-12 13:18:55 +0200

    fix NLTK_NBC to be compatible with python3 and pylint

--- TextClassification/NLTK_NBC.py
+++ TextClassification/NLTK_NBC.py
@@ -1,7 +1,4 @@
 import blockspring
-import json
-import sys
-import time
 import wikipedia
 from textblob import TextBlob
 from textblob.classifiers import NaiveBayesClassifier
@@ -12,22 +9,23 @@ Blockspring: https://open.blockspring.com/bs/get-wikipedia-articles-in-category
 TextBlob: http://textblob.readthedocs.org/en/dev/
 
 
-TextBlob has some problems for some non-ascii characters occuring in wikipedia articles. 
+TextBlob has some problems for some non-ascii
+characters occuring in wikipedia articles.
 All encoding changing in this file is done to remedy this.
 
 '''
 
-reload(sys)
-sys.setdefaultencoding('utf8')
+# reload(sys)
+# sys.setdefaultencoding('utf8')
 
 
 '''
 
-The methods here could definitely be improved, for example the 
-training methods, which should take a category list instead of several arguments,
+The methods here could definitely be improved, for example the training
+methods, which should take a category list instead of several arguments,
 and the testing methods which should also be generalized.
-However, as the NB classifier turned out to be very slow, we did not 
-spend any time on optimizing or refactoring other parts of the code as 
+However, as the NB classifier turned out to be very slow, we did not
+spend any time on optimizing or refactoring other parts of the code as
 the NLTK classifier was abandonded for other alternatives.
 
 
@@ -35,39 +33,40 @@ the NLTK classifier was abandonded for other alternatives.
 
 
 def createFile(category, numberOfArticles):
-	titleList = list(blockspring.runParsed("get-wikipedia-articles-in-category",{ "category": category, "limit": numberOfArticles }).params.values())
-	categoryList = titleList[0]
+    titleList = list(blockspring.runParsed("get-wikipedia-articles-in-category", {"category": category, "limit": numberOfArticles}).params.values())
+    categoryList = titleList[0]
+    articleFile = open(category, 'w+')
 
-	articleFile = open(category,'w+')
-	
-	for cat in categoryList:
-		ny = wikipedia.page(cat).content.encode('UTF-8')
-		articleFile.write(ny)
-		print("Loading article: " + cat)
-
+    for cat in categoryList:
+        nyy = wikipedia.page(cat).content.encode('UTF-8')
+        print('>>>>>>>>>>>>>>>>>>>>>', type(nyy))
+        ny = nyy.decode("utf-8")
+        print('>>>>>>>>>>>>>>>>>>>>>', type(ny))
+        articleFile.write(ny)
+        print("Loading article: " + cat)
 	articleFile.close
 
+
 def trainWithFullArticle(category, secondCategory):
-	with open(category, "r") as myFile:
-		text = myFile.read().replace("\n", " ")
-	
-	with open(secondCategory, "r") as myFile:
-		secondText = myFile.read().replace("\n", " ")
-	
-	train_data = [(text, category), (secondText, secondCategory) ]
-	print "Train data completed..."
+    with open(category, "r") as myFile:
+        text = myFile.read().replace("\n", " ")
+    with open(secondCategory, "r") as myFile:
+        secondText = myFile.read().replace("\n", " ")
 
-	print "Creating Naive Bayes classifier..."
-	cl = NaiveBayesClassifier(train_data) 
-	print "Naive Bayes finished"
+    train_data = [(text, category), (secondText, secondCategory)]
+    print("Train data completed...")
 
-	return cl
+    print("Creating Naive Bayes classifier...")
+    cl = NaiveBayesClassifier(train_data)
+    print("Naive Bayes finished")
+
+    return cl
 
 
 def trainWithNouns(category, secondCategory):
 	with open(category, "r") as myFile:
 		text = TextBlob(myFile.read().replace("\n", " "))
-	
+
 	with open(secondCategory, "r") as myFile:
 		secondText = TextBlob(myFile.read().replace("\n", " "))
 
@@ -79,29 +78,29 @@ def trainWithNouns(category, secondCategory):
 
 	train_data = firstList + secondList
 
-	print "Train data completed..."
+	print("Train data completed...")
 
-	print "Creating Naive Bayes classifier..."
+	print("Creating Naive Bayes classifier...")
 	cl = NaiveBayesClassifier(train_data)
-	print "Naive Bayes finished"
+	print("Naive Bayes finished")
 	return cl
 
 
 def testWithArticles(classifier):
 	testArticle = wikipedia.page("Integral").content
-	print classifier.classify(testArticle)
+	print(classifier.classify(testArticle))
 
 	testArticle = wikipedia.page("Special Relativity").content
-	print classifier.classify(testArticle)
+	print(classifier.classify(testArticle))
 
 	testArticle = wikipedia.page("Polynomial").content
-	print classifier.classify(testArticle)
+	print(classifier.classify(testArticle))
 
 	testArticle = wikipedia.page("Thermodynamics").content
-	print classifier.classify(testArticle)
+	print(classifier.classify(testArticle))
 
 	testArticle = wikipedia.page("Space").content
-	print classifier.classify(testArticle)
+	print(classifier.classify(testArticle))
 
 
 def testAccuracyOfWordClassification(classifier):
@@ -115,29 +114,30 @@ def testAccuracyOfWordClassification(classifier):
     	('Space', 'Physics'),
     	('Mechanics', 'Physics')
 	]
-	print classifier.accuracy(test)
+	print(classifier.accuracy(test))
 
 
-print "Creating files..."
+print("Creating files...")
 
 createFile("Physics", 2)
 
-print "First file created..."
+print("First file created...")
 
 createFile("Mathematics", 2)
 
-print "All files created..."
+print("All files created...")
 
 
 classifier = trainWithNouns("Physics", "Mathematics")
 
-print "Testing..."
+print("Testing...")
 
 
 testWithArticles(classifier)
 
 '''
 
-To test the NLTK classifier, just call any of the test methods above with the classifier.
+To test the NLTK classifier, just call any of the test
+methods above with the classifier.
 
 '''
